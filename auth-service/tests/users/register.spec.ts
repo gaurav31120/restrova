d3c02d1840837d9a7e513fcada8099303933955b
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-24 23:33:45 +0530

    test valid json

--- auth-service/tests/users/register.spec.ts
+++ auth-service/tests/users/register.spec.ts
@@ -1,28 +1,46 @@
-import request from 'supertest';
-import app from '../../src/app.js';
+import request from 'supertest'
+import app from '../../src/app.js'
 
-
-
-describe("POST/auth/register", () => {
-    describe("Given all fields", () => {
-        it("should return the 201 status code", async () => {
+describe('POST/auth/register', () => {
+    describe('Given all fields', () => {
+        it('should return the 201 status code', async () => {
             // AAA
             // Arrange
             const userData = {
-                firstName: "Gaurav",
-                lastName: "Kumar",
-                email: "gaurav@gmail.com",
-                password:"secret",
-
+                firstName: 'Gaurav',
+                lastName: 'Kumar',
+                email: 'gaurav@gmail.com',
+                password: 'secret',
             }
             // Act
-            const response = await request(app).post("/auth/register").send(userData);
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
             // console.log('Status Code:', response.statusCode);
             // console.log('Response Body:', response.body);
             // Assert
 
-            expect(response.statusCode).toBe(201); // This will fail and show Expected vs Received
+            expect(response.statusCode).toBe(201) // This will fail and show Expected vs Received
         })
-    });
-    describe("Fields are missing", () => {});
+
+        it('should return valid json response', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Gaurav',
+                lastName: 'Kumar',
+                email: 'gaurav@gmail.com',
+                password: 'secret',
+            }
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+            
+            // Assert application/json utf-8
+
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        })
+    })
+    describe('Fields are missing', () => {})
 })
